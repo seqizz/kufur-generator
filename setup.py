f4c56cc195d77623def5823aa6e35d40d9fd8dee
seqizz <seqizz@gmail.com>
2021-04-12 20:53:02 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+from glob import glob
+from sysconfig import get_paths
+
+setup(
+    name='kufur-generator',
+    version='1.0',
+    description='Turkce kufur ureteci',
+    author='seqizz',
+    include_package_data=True,
+    packages=["kufur-generator"],
+    data_files=[
+        ('{}/kufur-generator/data'.format(get_paths()['purelib']), glob('kufur-generator/data/*'))
+    ],
+)
