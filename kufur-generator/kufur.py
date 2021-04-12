850a56d73bed15d6e0a32353cbc7e42e6641176b
Gürkan <gitoperations@gurkan.in>
2021-04-12 20:53:02 +0200

    Add setup.py

--- kufur-generator/kufur.py
+++ kufur-generator/kufur.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
+#!/devel/kufur-generator/jj/bin/python3
 
 import random
+from os import path
 
 
 def find_one_random(inputfile):
-    with open(inputfile) as f:
+    with open('{}/data/{}'.format(path.dirname(path.realpath(__file__)), inputfile)) as f:
         lines = f.readlines()
         random_int = random.randint(0, len(lines) - 1)
         return lines[random_int].rstrip()
@@ -35,23 +36,23 @@ def format_suffix(direction, chosen_thing):
 def kufur():
     # Pick  the special full-sentence ones (lower probability)
     if bool(random.choices(population=[0, 1], weights=[4, 1], k=1)[0]):
-        return find_one_random('data/ozel_kufurler')
+        return find_one_random('ozel_kufurler')
 
     direction = not bool(random.getrandbits(1))
-    chosen_thing = find_one_random('data/hedefler')
+    chosen_thing = find_one_random('hedefler')
 
     # Add extra spice
     chosen_adjective = ''
     if bool(random.getrandbits(1)):
-        chosen_adjective = '{} '.format(find_one_random('data/sifatlar'))
+        chosen_adjective = '{} '.format(find_one_random('sifatlar'))
 
     return('{}{}{} {}{} {}'.format(
         'senin ' if bool(random.getrandbits(1)) else '',
         chosen_adjective,
-        find_one_random('data/kisiler'),
+        find_one_random('kisiler'),
         chosen_thing,
         format_suffix(direction, chosen_thing),
-        find_one_random('data/da-kufurler') if direction else find_one_random('data/a-kufurler'),
+        find_one_random('da-kufurler') if direction else find_one_random('a-kufurler'),
     ))
 
 
